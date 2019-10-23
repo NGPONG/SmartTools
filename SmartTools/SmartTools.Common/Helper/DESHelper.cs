cc102bbc0577653f0c7f3ca37829ea2988ad6839
NGPONG <1917648573@qq.com>
2019-10-23 21:41:52 +0800

    完成DES加密工具类

--- SmartTools/SmartTools.Common/Helper/DESHelper.cs
+++ SmartTools/SmartTools.Common/Helper/DESHelper.cs
@@ -10,30 +10,71 @@ namespace SmartTools.Common.Helper
 {
     public static class DESHelper
     {
-        private const string _KEY = "SMARTTOOLS_G";
-
-        public static string Encrypt(string sourceStr)
+        public static string Encrypt(string source,string _key)
         {
-            MemoryStream memStream;
-            CryptoStream crypStream;
+            MemoryStream output = null;
+            CryptoStream crypStream = null;
             try
             {
-                byte[] keyBytes = Encoding.UTF8.GetBytes(_KEY);
-                byte[] keyIV = keyBytes;
-                byte[] inputByteArray = Encoding.UTF8.GetBytes(sourceStr);
-                DESCryptoServiceProvider desProvider = new DESCryptoServiceProvider();
+                output = new MemoryStream();
 
-                // java 默认的是ECB模式，PKCS5padding；c#默认的CBC模式，PKCS7padding 所以这里我们默认使用ECB方式
-                desProvider.Mode = CipherMode.ECB;
-                memStream = new MemoryStream();
-                crypStream = new CryptoStream(memStream, desProvider.CreateEncryptor(keyBytes, keyIV), CryptoStreamMode.Write);
-                crypStream.Write(inputByteArray, 0, inputByteArray.Length);
+                byte[] rbgKey = Encoding.UTF8.GetBytes(_key), rbgIv = rbgKey;
+                byte[] input = Encoding.UTF8.GetBytes(source);
+
+                DESCryptoServiceProvider provider = new DESCryptoServiceProvider();
+                provider.Mode = CipherMode.ECB; // java 默认的是ECB模式 -> PKCS5padding  c#默认的CBC模式 -> PKCS7padding
+
+                crypStream = new CryptoStream(output, provider.CreateEncryptor(rbgKey, rbgIv), CryptoStreamMode.Write);
+                crypStream.Write(input, 0, input.Length);
                 crypStream.FlushFinalBlock();
-                return Convert.ToBase64String(memStream.ToArray(), Base64FormattingOptions.InsertLineBreaks);
+
+                return Convert.ToBase64String(output.ToArray(), Base64FormattingOptions.InsertLineBreaks);
             }
-            catch
+            catch(Exception objException)
             {
-                return sourceStr;
+                LogHelper.Error(objException);
+                return string.Empty;
+            }
+            finally
+            {
+                output.Close(); output.Dispose();
+                crypStream.Close(); crypStream.Dispose();
+            }
+        }
+
+        public static string Decrypt(string toDecrypt, string _key)
+        {
+            MemoryStream output = null;
+            CryptoStream crypStream = null;
+            try
+            {
+                byte[] input = new byte[toDecrypt.Length / 2];
+                for (int x = 0; x < toDecrypt.Length / 2; x++)
+                {
+                    int i = (Convert.ToInt32(toDecrypt.Substring(x * 2, 2), 16));
+                    input[x] = (byte)i;
+                }
+
+                DESCryptoServiceProvider provider = new DESCryptoServiceProvider();
+                provider.Key = ASCIIEncoding.ASCII.GetBytes(_key);
+                provider.IV = provider.Key;
+
+                output = new MemoryStream();
+                crypStream = new CryptoStream(output, provider.CreateDecryptor(), CryptoStreamMode.Write);
+                crypStream.Write(input, 0, input.Length);
+                crypStream.FlushFinalBlock();
+
+                return System.Text.Encoding.UTF8.GetString(output.ToArray());
+            }
+            catch (Exception objException)
+            {
+                LogHelper.Error(objException);
+                return string.Empty;
+            }
+            finally
+            {
+                output.Close(); output.Dispose();
+                crypStream.Close(); crypStream.Dispose();
             }
         }
     }
---
+++ SmartTools/SmartTools.Service/Licensing.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartTools.Service
+{
+    public static class Licensing
+    {
+        private const string __KEY = "SMARTTOOLS_G";
+    }
+}
