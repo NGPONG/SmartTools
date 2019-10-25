2e8ec8d07d8e240f074c0d2996dc00653456beb5
wupeng <1917648573@qq.com>
2019-10-25 17:30:23 +0800

    1111

--- SmartTools/SmartTools/Views/Login.Designer.cs
+++ SmartTools/SmartTools/Views/Login.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(816, 489);
             this.Name = "Login";
             this.ResumeLayout(false);
 
